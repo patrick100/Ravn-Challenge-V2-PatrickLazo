4dfa73da622ca115293b3f536982e07c1404eba9
patrick <patrickdz96@gmail.com>
2021-06-05 12:04:38 -0500

    fix info header

--- starwars/src/screens/Info.js
+++ starwars/src/screens/Info.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Text, View, Button } from 'react-native';
 import Detail from '../components/Detail/Detail.js';
 
 function Info({route, navigation}) {
 
   const {item} = route.params;
-  navigation.setOptions({ title: item.name })
+
+  useEffect(() => {
+    navigation.setOptions({ title: item.name })
+  },[])
 
   return(
     <Detail info={item}/>
